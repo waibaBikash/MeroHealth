aabaabbbcc923b8bcaf9016838dad253fc263d2f
waibaBikash <kavrelibikash@gmail.com>
2024-07-19 21:29:17 +1000

    save change

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,10 +1,10 @@
-
+import { Button } from '@/components/ui/button';
 
 export default function Home() {
   return (
     <div>
        <h1 className="text-white text-3xl underline">Home</h1>
-       <button>Click me</button>
+       <Button>Click me</Button>
     </div>
   );
 }
