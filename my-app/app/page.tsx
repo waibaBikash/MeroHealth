85e2f7380c2b2fb5dcc1f3f63b906f6ae1d3bd0f
waibaBikash <kavrelibikash@gmail.com>
2024-07-19 21:38:04 +1000

    save change

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -2,9 +2,12 @@ import { Button } from '@/components/ui/button';
 
 export default function Home() {
   return (
-    <div>
-       <h1 className="text-white text-3xl underline">Home</h1>
-       <Button>Click me</Button>
+    <div className='flex h-screen max-h-screen'>
+       <section className='remove-scrollbar container my-auto'>
+          <div className='sub-container max-w-[496px]'>
+
+          </div>
+       </section>
     </div>
   );
 }
