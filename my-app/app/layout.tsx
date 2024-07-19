bade8f841d85938ecc0167908d37708b88271663
waibaBikash <kavrelibikash@gmail.com>
2024-07-19 21:26:20 +1000

    commit change default

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Plus_Jakarta_Sans } from "next/font/google";
 import "./globals.css";
 import { cn } from '@/lib/utils';
+import { ThemeProvider } from "@/components/theme-provider"
 
 const fontSans = Plus_Jakarta_Sans({ 
   subsets: ["latin"],
@@ -24,9 +25,7 @@ export default function RootLayout({
       <body className={cn('min-h-screen bg-dark-300 font-sans antialiased', fontSans.variable)}>
       <ThemeProvider
             attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
+            defaultTheme="dark"
           >
             {children}
           </ThemeProvider>
