9124ca15950ee0b2577b51ab05cfcc07256d3c13
waibaBikash <kavrelibikash@gmail.com>
2024-07-19 21:23:46 +1000

    save change

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -21,7 +21,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cn('min-h-screen bg-dark-300 font-sans antialiased', fontSans.variable)}>{children}</body>
+      <body className={cn('min-h-screen bg-dark-300 font-sans antialiased', fontSans.variable)}>
+      <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        {children}
+        </body>
     </html>
   );
 }
