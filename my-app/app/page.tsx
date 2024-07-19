da33acc874b74aae952c87c3c8195cc448c3a6a7
waibaBikash <kavrelibikash@gmail.com>
2024-07-19 21:02:48 +1000

    shandcn install

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -3,7 +3,7 @@
 export default function Home() {
   return (
     <div>
-       <h1 className="text-3xl underline">Home</h1>
+       <h1 className="text-white text-3xl underline">Home</h1>
     </div>
   );
 }
