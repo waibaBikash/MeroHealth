70d6782b00d9df6489fa0c3eca2ecb4de0101e43
waibaBikash <kavrelibikash@gmail.com>
2024-07-20 22:03:53 +1000

    save change

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,11 +1,13 @@
-import { Button } from '@/components/ui/button';
+import Image from 'next/image'
 
 export default function Home() {
   return (
     <div className='flex h-screen max-h-screen'>
        <section className='remove-scrollbar container my-auto'>
           <div className='sub-container max-w-[496px]'>
-
+            <Image 
+              
+             />
           </div>
        </section>
     </div>
