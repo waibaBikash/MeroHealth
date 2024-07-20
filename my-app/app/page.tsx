0ec0cbfca58b521e8dc110ce4a53190c15337142
waibaBikash <kavrelibikash@gmail.com>
2024-07-20 22:42:40 +1000

    add icon

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -6,7 +6,12 @@ export default function Home() {
        <section className='remove-scrollbar container my-auto'>
           <div className='sub-container max-w-[496px]'>
             <Image
-               src='/assets'
+               src='/assets/icons/logo-full.svg'
+               height={1000}
+               width={1000}
+               alt='patient'
+               className='mb-12 h-10 w-fit'
+
              />
           </div>
        </section>
