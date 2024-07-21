da6a512e944116e6da4ea85259ea847b1735cc32
waibaBikash <kavrelibikash@gmail.com>
2024-07-21 14:31:35 +1000

    save change

--- my-app/components/forms/PatientFrom.tsx
+++ my-app/components/forms/PatientFrom.tsx
@@ -42,8 +42,8 @@ const PatientFrom =()=> {
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
          <section className="mb-12 space-y-4">
-            <h1>Hi there 👋</h1>
-            <p>Schedule your first appointment.</p>
+            <h1 className="header">Hi there 👋</h1>
+            <p className="text-dark-700">Schedule your first appointment.</p>
          </section>
         <FormField
           control={form.control}
