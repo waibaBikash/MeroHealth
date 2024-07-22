f7cd7c718a840796fb899915c3a8878540caf463
waibaBikash <kavrelibikash@gmail.com>
2024-07-22 20:49:47 +1000

    create userValidation

---
+++ my-app/lib/validation.ts
@@ -0,0 +1,12 @@
+import { z } from "zod";
+
+ 
+ export const UserFormValidation = z.object({
+  username: z.string()
+  .min(2, "Name must be at least 2 characters.")
+  .max(50, "Name must be most 50 characters."),
+  email: z.string().email("Invalid email address."),
+  phone: z.string().refine((phone) => /^\+\d{10,15}$/.test(phone),'Invalid phone number')
+  
+})
+ 
