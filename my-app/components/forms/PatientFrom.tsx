380a9105ffb1ba2dd387b22843c747aa22153082
waibaBikash <kavrelibikash@gmail.com>
2024-07-22 21:09:15 +1000

    create user function

--- my-app/components/forms/PatientFrom.tsx
+++ my-app/components/forms/PatientFrom.tsx
@@ -9,6 +9,7 @@ import CustomFormField from "../CustomFormField"
 import SubmitButton from "../SubmitButton"
 import { useState } from "react"
 import { UserFormValidation } from "@/lib/validation"
+import { useRouter } from "next/navigation";
 
 export enum FormFieldTye {
   INPUT = 'input',
@@ -22,6 +23,7 @@ export enum FormFieldTye {
  
 
 const PatientFrom =()=> {
+  const router =useRouter();
   // 1. Define your form.
   const [isLoading, setIsLoading] = useState(false);
   const form = useForm<z.infer<typeof UserFormValidation>>({
@@ -34,10 +36,19 @@ const PatientFrom =()=> {
   })
  
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof UserFormValidation>) {
+   async function onSubmit({name, email, phone}: z.infer<typeof UserFormValidation>) {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
-    console.log(values)
+    setIsLoading(true);
+     try {
+       const userData = { name, email, phone };
+       
+       const user = await createUser(userData);
+
+       if(user) router.push(`/patients/${user.$id}/register`)
+     } catch (error) {
+      console.log(error);
+     }
   }
   return (
     <Form {...form}>
