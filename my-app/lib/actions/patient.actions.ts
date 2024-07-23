f4144b6ad5b5d13669ff344658b963cf727bc2b9
waibaBikash <kavrelibikash@gmail.com>
2024-07-23 23:48:09 +1000

    commit change

--- my-app/lib/actions/patient.actions.ts
+++ my-app/lib/actions/patient.actions.ts
@@ -1,5 +1,6 @@
 import { ID, Query } from "node-appwrite"
 import { users } from "../appwrite.config"
+import { parseStringify } from '../utils';
 
 export const createUser = async (user: CreateUserParams) => {
   try {
@@ -10,12 +11,14 @@ export const createUser = async (user: CreateUserParams) => {
         undefined,
          user.name
         )
+        console.log({newUser})
+       return parseStringify(newUser);
+       
   } catch (error: any) {
     if(error && error ?.code === 409){
       const documents = await users.list([
         Query.equal('email', [user.email])
       ])
-      
       return documents?.users[0]
     }
   }
