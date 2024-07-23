029ba9f3b7b1ab612b1ba9ff9e016f86aa5e4515
waibaBikash <kavrelibikash@gmail.com>
2024-07-23 23:53:04 +1000

    save change

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -20,6 +20,7 @@ export default function RootLayout({
 }:Readonly<{
   children: React.ReactNode;
 }>) {
+  
   return (
     <html lang="en">
       <body className={cn('min-h-screen bg-dark-300 font-sans antialiased', fontSans.variable)}>
