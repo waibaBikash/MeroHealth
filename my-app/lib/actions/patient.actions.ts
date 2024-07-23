991d320ddd154c381944b7d7f66e650574dbb7f4
waibaBikash <kavrelibikash@gmail.com>
2024-07-23 23:27:18 +1000

    commit change

--- my-app/lib/actions/patient.actions.ts
+++ my-app/lib/actions/patient.actions.ts
@@ -1,3 +1,16 @@
-export const createUser = async (user: CreateUserParams) => {
+import { Query } from "node-appwrite"
+import { users } from "../appwrite.config"
 
+export const createUser = async (user: CreateUserParams) => {
+  try {
+    
+  } catch (error: any) {
+    if(error && error ?.code === 409){
+      const documents = await users.list([
+        Query.equal('email', [user.email])
+      ])
+      
+      return documents?.users[0]
+    }
+  }
 }
