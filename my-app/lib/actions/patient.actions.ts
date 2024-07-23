d7de077dbbc714f365abce5d83eee47ecaa2f8cf
waibaBikash <kavrelibikash@gmail.com>
2024-07-23 23:30:25 +1000

    save change

--- my-app/lib/actions/patient.actions.ts
+++ my-app/lib/actions/patient.actions.ts
@@ -1,9 +1,15 @@
-import { Query } from "node-appwrite"
+import { ID, Query } from "node-appwrite"
 import { users } from "../appwrite.config"
 
 export const createUser = async (user: CreateUserParams) => {
   try {
-    
+     const newUser = await users.create
+     (ID.unique(),
+      user.email,
+       user.phone,
+        undefined,
+         user.name
+        )
   } catch (error: any) {
     if(error && error ?.code === 409){
       const documents = await users.list([
