17eefd723c862fdad2129426cfcf4be928f9ac29
waibaBikash <kavrelibikash@gmail.com>
2024-07-24 16:46:50 +1000

    save change

--- my-app/app/layout.tsx
+++ my-app/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
           >
             {children}
           </ThemeProvider>
-        {children}
+        {/* {children} */}
         </body>
     </html>
   );
