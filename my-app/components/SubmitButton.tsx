a458d8c6ad6689f3319fae6d172c0ede7b110acf
waibaBikash <kavrelibikash@gmail.com>
2024-07-24 20:38:51 +1000

    commit change

--- my-app/components/SubmitButton.tsx
+++ my-app/components/SubmitButton.tsx
@@ -8,7 +8,7 @@ interface ButtonProps {
   children: React.ReactNode,
 }
 
-const SubmitButton = ({isLoading, className, children }: ButtonProps) => {
+const SubmitButton = ({ isLoading, className, children }: ButtonProps) => {
   return (
     <Button type="submit" disabled={isLoading} className={className ?? 'shad-primary-btn w-full'} >
       {isLoading ? (
