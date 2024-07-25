df17600bbef179bffa1096c70b38ddfc093b57d2
waibaBikash <kavrelibikash@gmail.com>
2024-07-25 21:31:59 +1000

    create a register page

---
+++ my-app/app/patients/[userId]/register/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Register = () => {
+  return (
+    <div className='flex h-screen max-h-screen'>
+       <section>
+          <div>
+            
+          </div>
+       </section>
+
+    </div>
+  )
+}
+
+export default Register;
