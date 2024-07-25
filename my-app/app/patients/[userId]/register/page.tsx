886696a968debcd02810f698cb94919e2deca790
waibaBikash <kavrelibikash@gmail.com>
2024-07-25 22:29:31 +1000

    complete register form

--- my-app/app/patients/[userId]/register/page.tsx
+++ my-app/app/patients/[userId]/register/page.tsx
@@ -1,12 +1,41 @@
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react'
 
 const Register = () => {
   return (
     <div className='flex h-screen max-h-screen'>
-       <section>
-          <div>
-            
+       <section className='remove-scrollbar container'>
+       <div className='flex h-screen max-h-screen'>
+      {/* TODO: OTP Verification | PasskeyModel */}
+       <section className='remove-scrollbar container my-auto'>
+          <div className='sub-container max-w-[496px]'>
+            <Image
+               src='/assets/icons/logo-full.svg'
+               height={1000}
+               width={1000}
+               alt='patient'
+               className='mb-12 h-10 w-fit'
+             />
+             {/* <PatientFrom /> */}
+              <div className='text-14-regular mt-20 flex justify-between'>
+                <p className='justify-items-end text-dark-600 xl:text-left'>
+                © 2024 CarePulse
+                </p>
+                <Link href='/?admin=true' className='text-green-500' >
+                  Admin
+                </Link>
+              </div>
           </div>
+       </section>
+       <Image 
+         src='/assets/images/register-img.png'
+         height={1000}
+         width={1000}
+         alt='patient'
+         className='side-img max-w-[390px]'
+       />
+    </div>
        </section>
 
     </div>
