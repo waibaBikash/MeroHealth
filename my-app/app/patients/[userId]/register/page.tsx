82226bb011896971ebd96ca0e41a13bf0ea73d4b
waibaBikash <kavrelibikash@gmail.com>
2024-07-25 22:35:38 +1000

    save change

--- my-app/app/patients/[userId]/register/page.tsx
+++ my-app/app/patients/[userId]/register/page.tsx
@@ -1,3 +1,4 @@
+import RegisterForm from '@/components/forms/RegisterForm';
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react'
@@ -17,7 +18,9 @@ const Register = () => {
                alt='patient'
                className='mb-12 h-10 w-fit'
              />
-             {/* <PatientFrom /> */}
+
+             <RegisterForm />
+             
               <div className='text-14-regular mt-20 flex justify-between'>
                 <p className='justify-items-end text-dark-600 xl:text-left'>
                 © 2024 CarePulse
