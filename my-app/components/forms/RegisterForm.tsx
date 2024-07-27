9348a9b7b73c322fe809de6ff7ecd8f22ae4db7b
waibaBikash <kavrelibikash@gmail.com>
2024-07-27 21:53:33 +1000

    update medical information

--- my-app/components/forms/RegisterForm.tsx
+++ my-app/components/forms/RegisterForm.tsx
@@ -103,6 +103,14 @@ const RegisterForm = ({ user }: { user: User }) => {
             )}
          />
          </div>
+          
+         <section className="space-y-6">
+           <div className="mb-9 space-y-1">
+
+           </div>
+            <h2 className="sub-header">Medical Information</h2>
+         </section>
+
          <div className="flex flex-col gap-6 xl:flex-row">
          </div>
 
