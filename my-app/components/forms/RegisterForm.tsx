ba8f8558e23e3cd149a7c8f2d569fde2e75e54ce
waibaBikash <kavrelibikash@gmail.com>
2024-07-27 21:44:17 +1000

    commit change

--- my-app/components/forms/RegisterForm.tsx
+++ my-app/components/forms/RegisterForm.tsx
@@ -88,7 +88,7 @@ const RegisterForm = ({ user }: { user: User }) => {
           label='Gender'
           renderSkeleton={(field) => (
             <FormControl>
-             <RadioGroup className="flex h-11 xl:justify-between" onValueChange={field.onChange} defaultValue={field.value}>
+             <RadioGroup className="flex h-11 gap-6 xl:justify-between" onValueChange={field.onChange} defaultValue={field.value}>
               {GenderOptions.map((option) => (
                 <div key={option} className="radio-group">
                   <RadioGroupItem value={option} id={option}/>
