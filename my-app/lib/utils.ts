10c1c8b44a70d6efc4ca83c6aa8b100d707849c0
waibaBikash <kavrelibikash@gmail.com>
2024-07-29 20:50:35 +1000

    commit change

--- my-app/lib/utils.ts
+++ my-app/lib/utils.ts
@@ -78,7 +78,6 @@ export const formatDateTime = (dateString: Date | string) => {
 export function encryptKey(passkey: string) {
   return btoa(passkey);
 }
-
 export function decryptKey(passkey: string) {
   return atob(passkey);
 }
