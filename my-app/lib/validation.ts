5c66830a8203f4d88448f8bedc34db2327836f87
waibaBikash <kavrelibikash@gmail.com>
2024-07-29 20:53:07 +1000

    save change

--- my-app/lib/validation.ts
+++ my-app/lib/validation.ts
@@ -2,6 +2,7 @@ import { z } from "zod";
 
  
  export const UserFormValidation = z.object({
+  
   name: z.string()
   .min(2, "Name must be at least 2 characters.")
   .max(50, "Name must be most 50 characters."),
