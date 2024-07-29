8a6d7e292c6c31652a7c59432692077e863d1263
waibaBikash <kavrelibikash@gmail.com>
2024-07-29 20:50:19 +1000

    save change

--- my-app/lib/appwrite.config.ts
+++ my-app/lib/appwrite.config.ts
@@ -13,7 +13,6 @@ client
 .setProject(PROJECT_ID!)
 .setKey(API_KEY!);
 
-
 export const databases = new sdk.Databases(client);
 export const storage = new sdk.Storage(client);
 export const messaging = new sdk.Messaging(client);
