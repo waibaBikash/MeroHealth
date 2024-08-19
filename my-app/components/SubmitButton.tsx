d4b87383f51f17ef9a8e036d8d56c04a1ec2bb95
waibaBikash <kavrelibikash@gmail.com>
2024-08-19 19:11:31 +1000

    save change

--- my-app/components/SubmitButton.tsx
+++ my-app/components/SubmitButton.tsx
@@ -9,7 +9,10 @@ interface ButtonProps {
 }
 const SubmitButton = ({ isLoading, className, children }: ButtonProps) => {
   return (
-    <Button type="submit" disabled={isLoading} className={className ?? 'shad-primary-btn w-full'} >
+    <Button 
+     type="submit" 
+     disabled={isLoading} 
+     className={className ?? 'shad-primary-btn w-full'} >
       {isLoading ? (
         <div className='flex items-center gap-4'>
             <Image 
