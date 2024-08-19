5646578aedc1caca7250367e0e3ed1d2f5a8110d
waibaBikash <kavrelibikash@gmail.com>
2024-08-19 19:06:47 +1000

    commit change

--- my-app/components/forms/RegisterForm.tsx
+++ my-app/components/forms/RegisterForm.tsx
@@ -63,7 +63,6 @@ const RegisterForm = ({ user }: { user: User }) => {
          </section>
          <section className="space-y-6">
            <div className="mb-9 space-y-1">
-
            </div>
             <h2 className="sub-header">Personal Information</h2>
          </section>
