2d8d65cb7367bf8434201cab30ea7189411dc381
waibaBikash <kavrelibikash@gmail.com>
2024-08-19 19:09:50 +1000

    save change

--- my-app/components/forms/PatientFrom.tsx
+++ my-app/components/forms/PatientFrom.tsx
@@ -50,7 +50,9 @@ const PatientFrom = () => {
   }
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
+      <form 
+          onSubmit={form.handleSubmit(onSubmit)} 
+          className="space-y-6 flex-1">
          <section className="mb-12 space-y-4">
             <h1 className="header">Hi there 👋</h1>
             <p
