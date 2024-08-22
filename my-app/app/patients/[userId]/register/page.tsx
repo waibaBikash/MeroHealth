053374d938f3f03292696e88c4287f4a32e2448e
waibaBikash <kavrelibikash@gmail.com>
2024-08-22 19:15:54 +1000

    changes made

--- my-app/app/patients/[userId]/register/page.tsx
+++ my-app/app/patients/[userId]/register/page.tsx
@@ -2,7 +2,7 @@ import RegisterForm from '@/components/forms/RegisterForm';
 import { getUser } from '@/lib/actions/patient.actions';
 import Image from 'next/image';
 import Link from 'next/link';
-import React from 'react'
+import React from 'react';
 
 const Register = async ({params: {userId}}: SearchParamProps) => {
 
