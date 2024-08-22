1f6de34ca96b798a6d36be14e6f1261a59796db9
waibaBikash <kavrelibikash@gmail.com>
2024-08-22 19:17:41 +1000

    save change

--- my-app/lib/actions/patient.actions.ts
+++ my-app/lib/actions/patient.actions.ts
@@ -26,7 +26,6 @@ export const createUser = async (user: CreateUserParams) => {
 export const getUser = async (userId: string) => {
   try {
     const user = await users.get(userId);
-
     return parseStringify(user);
   } catch (error) {
     console.log(error);
